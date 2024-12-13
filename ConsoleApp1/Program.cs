3e75e3bbcc7f4768704fc4550d8ed37c572545d7
MiloszGit <milosz.biniek.1995@gmail.com>
2024-12-13 21:35:40 +0100

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4, b = 1, wynik;
+            wynik = 2 * a - 3 * b;
+            Console.WriteLine(wynik);
+            Console.ReadKey();
+        }
+    }
+}
